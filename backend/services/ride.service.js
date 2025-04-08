23d8c3fcc1504a8262daac43de69ca917fa87d14
AshutoshMadhesiya <114347327+AshutoshMadhesiya@users.noreply.github.com>
2025-04-09 02:13:45 +0530

    feature: add ride creation functionality with routes, controller, model, and service

---
+++ backend/services/ride.service.js
@@ -0,0 +1,73 @@
+const rideModel = require('../models/ride.model');
+const crypto = require('crypto');
+const mapService = require('./map.service');
+
+
+async function getFare(pickup,destination){
+  
+  if (!pickup || !destination) {
+    throw new Error('Pickup and destination locations are required');
+  }
+  
+  const distanceTime = await mapService.getDistanceTime(pickup, destination);
+
+  const distanceInKm = parseFloat(distanceTime.distance);
+  const timeInMin = parseFloat(distanceTime.duration);
+  
+  const baseFare = {
+    "car": 50,
+    "motorcycle": 20,
+    "auto": 30,
+  }
+  
+  const perKmRate = {
+    "car": 10,
+    "motorcycle": 5,
+    "auto": 7,
+  }
+  
+  const perMinuteRate={
+    "car": 2.5,
+    "motorcycle": 1.5,
+    "auto": 2.0,
+  }
+  
+  const fare = {
+    car: Math.round(baseFare.car + (perKmRate.car * distanceInKm) + (perMinuteRate.car * timeInMin)),
+    motorcycle: Math.round(baseFare.motorcycle + (perKmRate.motorcycle * distanceInKm) + (perMinuteRate.motorcycle * timeInMin)),
+    auto: Math.round(baseFare.auto + (perKmRate.auto * distanceInKm) + (perMinuteRate.auto * timeInMin)),
+  };
+  
+  
+  return fare;
+  
+}
+
+function getOtp(length) {
+  const otp = crypto.randomInt(Math.pow(10, length - 1), Math.pow(10, length) - 1).toString();
+  return otp;
+}
+
+module.exports.createRide = async (user,pickup,destination,vehicleType) => {
+
+  if(!user || !pickup || !destination || !vehicleType) {
+    throw new Error('User, pickup, destination and vehicle type are required');
+  }
+
+  const fare = await getFare(pickup, destination);
+
+  const ride = {
+    user,
+    pickup,
+    destination,
+    otp: getOtp(6),
+    fare: fare[vehicleType],
+  };
+
+  const createdRide = await rideModel.create(ride);
+  return createdRide;
+ 
+}
+
+
+
